5d6cbe13d7027aa3edf24e08738812052c7670f6
matthias-matthies <m.matthies@uni-bremen.de>
2023-05-24 17:21:28 +0200

    Automatically get current semester

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,17 @@ import heroImage from "@/public/developer.png"
 import Link from "next/link";
 
 export default function Home() {
-    const currentSemester: Number = 2
+    const currentSemester = (startDate: Date): Number => {
+        const today = new Date()
+
+        const yearsPassed = (today.getFullYear() - startDate.getFullYear())
+        const monthsPassed = (today.getMonth() - startDate.getMonth() + 1)
+
+        const totalMonthsPassed = (yearsPassed * 12) + monthsPassed
+
+        return Math.ceil(totalMonthsPassed / 6)
+    }
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <div className='px-16 flex items-center justify-between h-screen'>
@@ -18,7 +28,7 @@ export default function Home() {
                         text='Matthias Matthies, Informatik Student, Fullstack-Entwickler, Sport-Enthusiast'
                     />
                     <p>
-                        Moin! Mein Name ist Matthias und ich befinde mich aktuell im <b>{currentSemester.toString()}</b>.
+                        Moin! Mein Name ist Matthias und ich befinde mich aktuell im <b>{currentSemester(new Date(2022, 10)).toString()}</b>.
                         Semester meines Informatikstudiums an der <Link target="_blank" className="text-blue-500" href="https://www.uni-bremen.de">Universität Bremen</Link>. Zusätzlich arbeite ich als Entwickler bei der <Link className="text-blue-500" href="https://www.k-evolution.de" target="_blank">K-Evolution GmbH</Link> in Sittensen.
                         In meiner Freizeit widme ich mich gerne sportlichen Aktivitäten und arbeite nebenbei an einigen privaten <Link className="text-blue-500" href="/projects">Programmierprojekten</Link>.
                     </p>
