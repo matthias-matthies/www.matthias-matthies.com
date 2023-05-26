360edbc774d03605b3f581b15278e3c07e505ab6
matthias-matthies <m.matthies@uni-bremen.de>
2023-05-26 09:55:53 +0200

    Andere transition

--- app/components/TransitionEffect.tsx
+++ app/components/TransitionEffect.tsx
@@ -7,27 +7,27 @@ const TransitionEffect: React.FC = () => {
     /*return (
         <>
             <motion.div
-                className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-blue-500"
+                className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-black"
                 initial={{x: "100%", width: "100%"}}
                 animate={{x: "0%", width: "0%"}}
-                transition={{duration: 0.8, ease: "easeInOut"}}
+                transition={{duration: 0.6, ease: "easeInOut"}}
             />
             <motion.div
                 className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-white"
                 initial={{x: "100%", width: "100%"}}
                 animate={{x: "0%", width: "0%"}}
-                transition={{delay: 0.2, duration: 0.8, ease: "easeInOut"}}
+                transition={{delay: 0.2, duration: 0.6, ease: "easeInOut"}}
             />
             <motion.div
-                className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-black"
+                className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-blue-500"
                 initial={{x: "100%", width: "100%"}}
                 animate={{x: "0%", width: "0%"}}
-                transition={{delay: 0.4, duration: 0.8, ease: "easeInOut"}}
+                transition={{delay: 0.4, duration: 0.6, ease: "easeInOut"}}
             />
         </>
     )*/
 
-    return (
+    /*return (
         <>
             <motion.div
                 className="fixed left-0 right-0 bottom-full w-screen h-screen z-30 bg-blue-500"
@@ -48,6 +48,23 @@ const TransitionEffect: React.FC = () => {
                 initial={{y: "100%", height: "100%"}}
                 animate={{y: "0%", height: "0%"}}
                 transition={{delay: 0.4, duration: 0.8, ease: "easeInOut"}}
+            />
+        </>
+    )*/
+
+    return (
+        <>
+            <motion.div
+                className="fixed top-0 left-0 bottom-0 right- 0 w-screen h-screen z-30 bg-blue-500"
+                initial={{clipPath: "polygon(0 0, 0 100%, 100% 0)"}}
+                animate={{clipPath: "polygon(0 0, 0 0, 0 0)"}}
+                transition={{duration: 0.8, ease: "easeInOut"}}
+            />
+            <motion.div
+                className="fixed top-0 left-0 bottom-0 right- 0 w-screen h-screen z-30 bg-blue-500"
+                initial={{clipPath: "polygon(100% 100%, 0% 100%, 100% 0)"}}
+                animate={{clipPath: "polygon(100% 100%, 100% 100%, 100% 100%)"}}
+                transition={{duration: 0.8, ease: "easeInOut"}}
             />
         </>
     )
