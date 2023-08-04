e1658546aa8ff4b1d63f8446adad9321d8de8a6a
matthias-matthies <matthias.matthies.business@gmail.com>
2023-08-04 14:25:24 +0200

    Verbose handling

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+    serverRuntimeConfig: {
+        VERCEL_ENV: process.env.VERCEL_ENV,
+        VERCEL_URL: process.env.VERCEL_URL,
+        NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    },
+    publicRuntimeConfig: {
+        // Add your public runtime configurations here
+    },
+    webpack(config) {
+        config.resolve.modules.push(__dirname);
+        return config;
+    },
+};
