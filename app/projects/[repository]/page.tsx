b7f3673b2d3e7f5b1e3fcf45090704b79a0e9b57
matthias-matthies <matthias.matthies.business@gmail.com>
2023-08-04 14:50:33 +0200

    Test ob animatedheading workt

--- app/projects/[repository]/page.tsx
+++ app/projects/[repository]/page.tsx
@@ -1,6 +1,4 @@
 "use client"
-
-import AnimatedHeading from '@/app/components/AnimatedHeading'
 import {compileCustomMDX} from "@/app/lib/compileCustomMDX";
 
 interface ProjectPageParams {
@@ -26,12 +24,3 @@ export default async function ProjectPage({ params }: { params: ProjectPageParam
         </main>
     )
 }
-
-// Für Name: Name
-// Für Link: html_url
-// Für Beschreibung: description
-// Für Blogpost Markdown oder JSON oder whatnot = https://raw.githubusercontent.com/matthias-matthies/vercel-matthias-matthies/main/.matthias-matthies/BLOG.md or .json // Muss ich mir noch überlegen, wie ich meine Repos nutzen will.
-// https://raw.githubusercontent.com/[user]/[repo]/main/[path]
-// Für Tags: fetch(tags_url)
-// Für GitTags: fetch(tags_url)
-// Für Repo direkt fetchen fetch(url) url
