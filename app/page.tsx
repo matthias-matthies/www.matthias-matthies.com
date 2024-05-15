0ddbd529e8c9223c33a44b8c9b048c788feb0a77
Matthias Matthies <81367157+matthias-matthies@users.noreply.github.com>
2024-05-15 11:22:29 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
                         Matthias Matthies, Informatik Student, Fullstack-Entwickler, Sport-Enthusiast
                     </AnimatedHeading>
                     <p>
-                        Moin! Mein Name ist Matthias und ich befinde mich aktuell im <b>{currentSemester(new Date(2022, 9)).toString()}</b>.
+                        Moin! Mein Name ist Matthias und ich befinde mich aktuell im <b>4{/*currentSemester(new Date(2022, 9)).toString()*/}</b>.
                         Semester meines Informatikstudiums an der <Link target="_blank" className="text-blue-500" href="https://www.uni-bremen.de">Universität Bremen</Link>. Zusätzlich arbeite ich als Entwickler bei der <Link className="text-blue-500" href="https://www.k-evolution.de" target="_blank">K-Evolution GmbH</Link> in Sittensen.
                         In meiner Freizeit widme ich mich gerne sportlichen Aktivitäten und arbeite nebenbei an einigen privaten <Link className="text-blue-500" href="/projects">Programmierprojekten</Link>.
                     </p>
